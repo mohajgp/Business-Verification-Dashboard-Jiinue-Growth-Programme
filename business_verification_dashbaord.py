d4ec061934cfc3b5706a970c9b4021d718e5be63
mohajgp <abdullahi.hussein@kenyachamber.or.ke>
2025-08-20 16:20:33 +0300

    Update business_verification_dashbaord.py

--- business_verification_dashbaord.py
+++ business_verification_dashbaord.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
 # -------------------- PAGE CONFIG --------------------
